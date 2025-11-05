9320634fb244af7b2eba22838ec8510f3c1840dc
Alexandre Selani <xandyselani@outlook.com>
2025-11-05 09:56:21 -0300

    stratified k fold

--- decision_tree.py
+++ decision_tree.py
@@ -1,60 +1,34 @@
 from sklearn.preprocessing import LabelEncoder
 from sklearn import tree
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,StratifiedKFold
 from sklearn.metrics import f1_score,accuracy_score
 import pandas as pd
 import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')  
 import matplotlib.pyplot as plt
+
 """Código para treinar e testar uma arvore de decisao para detectar ataques de rede.
 
 Baseado no dataset IoT intrusion (https://www.kaggle.com/datasets/subhajournal/iotintrusion)"""
 
 def loadData(data):
 
-    labels = data.iloc[:,-1]
+    y = data.iloc[:,-1]
 
-    loaded_data = data.iloc[:,:-1]
-    print(labels.unique())
-    X_train,X_test,y_train,y_test = train_test_split(loaded_data,labels,test_size=0.20)
-    return X_train,X_test,y_train,y_test
+    X = data.iloc[:,:-1]
+    
+    skf = StratifiedKFold(n_splits=5,shuffle=True,random_state=42)
+
+    return skf,X,y
+
 
 def saveTreeImage(decision_tree):
     plt.figure(figsize=(50,50))
-    tree.plot_tree(decision_tree, max_depth=10, filled=True, fontsize=8)
-    plt.savefig("arvore_reduzida.png", dpi=300, bbox_inches="tight")
+    tree.plot_tree(decision_tree, max_depth=3, filled=True, fontsize=10)
+    plt.savefig("arvore_exemplo.png", dpi=300, bbox_inches="tight")
 
-if __name__ == "__main__":
-    #-----------------Carregamento de dados e pré-processamento
-    try:
-        raw_data = pd.read_csv('IoT_Intrusion.csv')
-    except Exception as e:
-        print("Erro ao carregar arquivo")
-        exit()
-
-
-    X_train,X_test,y_train,y_test = loadData(raw_data)
-
-    label_encoder = LabelEncoder()
-    y_train = label_encoder.fit_transform(y_train)
-    y_test = label_encoder.fit_transform(y_test)
-    
-
-    #------------------Treinamento + teste e avaliação
-    min_samples_split=100
-    decision_tree = tree.DecisionTreeClassifier()
-    decision_tree.fit(X_train,y_train)
-
-    saveTreeImage(decision_tree)
-
-    predicts = decision_tree.predict(X_test)
-    F1_score = f1_score(y_test, predicts,average='macro')
-    acc = accuracy_score(y_test, predicts)
-
-    print(f"accuracy:{acc}\nF1 score = {F1_score}")
-
-    #----------------Importancia das features
+def featureImportances(decision_tree):
 
     feature_importances = decision_tree.feature_importances_
     
@@ -66,6 +40,51 @@ if __name__ == "__main__":
 
     print(importance_df)
 
+if __name__ == "__main__":
 
+    #-----------------Carregamento de dados e pré-processamento
+    try:
+        raw_data = pd.read_csv('IoT_Intrusion.csv')
+    except Exception as e:
+        print("Erro ao carregar arquivo")
+        exit()
+
+
+    skf,X,y = loadData(raw_data)
+
+    label_encoder = LabelEncoder()
+    y = label_encoder.fit_transform(y)
+    
+    #------------------Treinamento + teste e avaliação (usando Stratified k-fold cross validation)
+    min_samples_split=100
+    accs,f1s = np.array([]),np.array([])
+    
+    for i, (train_idx, test_idx) in enumerate(skf.split(X,y)):
+        X_train = X.iloc[train_idx,:]
+        y_train = y[train_idx]
+
+        X_test = X.iloc[test_idx,:]
+        y_test = y[test_idx]
+
+        decision_tree = tree.DecisionTreeClassifier()
+        decision_tree.fit(X_train,y_train)
+
+        if(i==0):
+            saveTreeImage(decision_tree)
+
+        predicts = decision_tree.predict(X_test)
+
+        F1_score = f1_score(y_test, predicts,average='macro')
+        acc = accuracy_score(y_test, predicts)
+
+        f1s = np.append(f1s,F1_score)
+        accs = np.append(acc,acc)
+        print(f"FOLD {i}\nAccuracy = {acc}\nF1 score = {F1_score}\n-------------------------")
+
+    
+        #----------------Importancia das features
+        #featureImportances(decision_tree)
+
+    print(f"Resultados finais:\nAccuracy = {accs.mean():.3f}\nF1 score = {f1s.mean():.3f}")
 
 
